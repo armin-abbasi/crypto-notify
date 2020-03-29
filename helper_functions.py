2fb215eb3f1b4f685b9f9701ed5e1e1ba1201d90
Armin Abbasi <armin.abbasi@hotmail.com>
2020-03-29 21:51:27 +0430

    Adds check_range function to helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -7,3 +7,10 @@ def env(variable_name: str) -> str:
         key, value = item.split('=')
         if key == str(variable_name).strip():
             return str(value).strip()
+
+
+# Check if the given number passes threshold or not
+def check_range(number: int, min: int, max: int) -> bool:
+    if number < min or number > max:
+        return True
+    return False
