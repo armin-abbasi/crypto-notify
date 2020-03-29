4c246a157e9eb3a716743c9f59460e94e579a13c
Armin Abbasi <armin.abbasi@hotmail.com>
2020-03-29 21:22:15 +0430

    Remove whitespace from .env variables

--- helper_functions.py
+++ helper_functions.py
@@ -1,9 +1,9 @@
 # Read environment variables from .env file
-def env(variable_name: str):
+def env(variable_name: str) -> str:
     env_file = open('.env')
     variables = env_file.readlines()
 
     for item in variables:
         key, value = item.split('=')
         if key == variable_name:
-            return value
+            return str(value).strip()
